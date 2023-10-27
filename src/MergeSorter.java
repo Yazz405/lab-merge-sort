ff64f119a453707891f93a4294d3f0df390aa06f
Alma <almastar405@gmail.com>
2023-10-27 09:41:51 -0500

    started

--- src/MergeSorter.java
+++ src/MergeSorter.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
@@ -31,7 +32,28 @@ public class MergeSorter {
    * Preconditions: Each subarray is sorted accorting to comparator.
    */
   static <T> void merge(T[] vals, int lo, int mid, int hi, Comparator<? super T> comparator) {
-    // STUB
+    T[] temp =  Arrays.copyOfRange(vals , lo, hi);
+
+    for(int i = 0; i < vals.length; i++){
+      if(comparator.compare(vals[lo], vals[hi]) <= 0){
+        temp[i] = vals[lo];
+        lo++;
+      }//if
+      else if(comparator.compare(vals[lo], vals[hi]) > 0){
+        temp[i] = vals[hi];
+        hi++;
+      }//else if
+      else if(lo == mid){
+        temp[i] = vals[hi];
+      }//else if
+      else if(hi == vals.length - 1){
+        temp[i] = vals[lo];
+      }//else if
+    }//for
+
+    for(int i = 0; i < vals.length; i++){
+      vals[i] = temp[i];
+    }//for
   } // merge
 
 } // class MergeSorter
